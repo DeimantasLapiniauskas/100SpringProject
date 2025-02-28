aab0cace631f8420117b2fc15f14dcab2c2da49c
vejas <vejasva@gmail.com>
2025-02-28 10:34:02 +0200

    added add service

---
+++ api/src/main/java/SpringProject/_Spring/service/ServiceAtClinicService.java
@@ -0,0 +1,28 @@
+package SpringProject._Spring.service;
+
+import SpringProject._Spring.model.ServiceAtClinic;
+import SpringProject._Spring.repository.ServiceAtClinicRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+import java.util.List;
+
+@Service
+public class ServiceAtClinicService {
+    private final ServiceAtClinicRepository serviceAtClinicRepository;
+
+    @Autowired
+
+    public ServiceAtClinicService(ServiceAtClinicRepository serviceAtClinicRepository) {
+        this.serviceAtClinicRepository = serviceAtClinicRepository;
+    }
+
+    public boolean existsServiceByName(String name) {
+        return serviceAtClinicRepository.existsByName(name);
+    }
+
+    public ServiceAtClinic saveService(ServiceAtClinic service) {
+        return serviceAtClinicRepository.save(service);
+    }
+}
