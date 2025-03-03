e72f6990faf961ab549075b4f457a37487773497
Nikas <d2jp@TECHIN.INTERNAL>
2025-03-03 13:38:30 +0200

    Add delete more validation

--- api/src/main/java/SpringProject/_Spring/dto/ServiceAtClinicRequestDTO.java
+++ api/src/main/java/SpringProject/_Spring/dto/ServiceAtClinicRequestDTO.java
@@ -1,7 +1,10 @@
 package SpringProject._Spring.dto;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import org.hibernate.annotations.NotFound;
 
 import java.math.BigDecimal;
 
@@ -10,7 +13,10 @@ public record ServiceAtClinicRequestDTO(
         @Size(min = 3, max = 150)
         String name,
         @NotBlank
+        @NotNull
         String description,
+        @NotNull
+        @Min(1)
         BigDecimal price
 ) {
 }
