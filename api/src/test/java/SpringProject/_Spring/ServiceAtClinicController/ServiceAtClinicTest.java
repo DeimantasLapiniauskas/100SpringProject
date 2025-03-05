e0cc49f53acfeba631ed32a8d355548035aba14c
DeimantasLapiniauskas <deimislapinas@gmail.com>
2025-03-05 08:25:09 +0200

    Fixed KOM-79 and KOM-81

--- api/src/test/java/SpringProject/_Spring/ServiceAtClinicController/ServiceAtClinicTest.java
+++ api/src/test/java/SpringProject/_Spring/ServiceAtClinicController/ServiceAtClinicTest.java
@@ -33,83 +33,160 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebMvcTest(controllers = ServiceAtClinicController.class)
 @Import(SecurityConfig.class)
 public class ServiceAtClinicTest {
-  @MockitoBean
-  private ServiceAtClinicService service;
-  @Autowired
-  private MockMvc mockMvc;
+    @MockitoBean
+    private ServiceAtClinicService service;
+    @Autowired
+    private MockMvc mockMvc;
 
-  @Test
-  @WithMockUser
-  void saveServiceTest() throws Exception{
-    ServiceAtClinic serviceAtClinic = new ServiceAtClinic("some", "good service", new BigDecimal("10.5"));
+    @Test
+    @WithMockUser
+    void saveServiceTest() throws Exception {
+        ServiceAtClinic serviceAtClinic = new ServiceAtClinic("some", "good service", new BigDecimal("10.5"));
 
-    BDDMockito.given(service.saveService(ArgumentMatchers.any(ServiceAtClinic.class))).willReturn(serviceAtClinic);
+        BDDMockito.given(service.saveService(ArgumentMatchers.any(ServiceAtClinic.class))).willReturn(serviceAtClinic);
 
-    ObjectMapper objectMapper = new ObjectMapper();
+        ObjectMapper objectMapper = new ObjectMapper();
 
-    mockMvc.perform(MockMvcRequestBuilders.post("/api/services")
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .content(objectMapper.writeValueAsString(serviceAtClinic)))
-            .andExpect(status().isCreated())
-            .andExpect(jsonPath("id").exists())
-            .andExpect(jsonPath("name").value("some"));
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/services")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(serviceAtClinic)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("id").exists())
+                .andExpect(jsonPath("name").value("some"));
 
-    Mockito.verify(service, Mockito.times(1)).saveService(ArgumentMatchers.any(ServiceAtClinic.class));
-  }
+        Mockito.verify(service, Mockito.times(1)).saveService(ArgumentMatchers.any(ServiceAtClinic.class));
+    }
 
-  @Test
-  @WithMockUser
-  void saveServiceBadTest() throws Exception{
-    ServiceAtClinic serviceAtClinic = new ServiceAtClinic("", "good service", new BigDecimal("10.5"));
+    @Test
+    @WithMockUser
+    void saveServiceShortNameTest() throws Exception {
+        ServiceAtClinic serviceAtClinic = new ServiceAtClinic("a", "good service", new BigDecimal("10.5"));
 
-    BDDMockito.given(service.saveService(ArgumentMatchers.any(ServiceAtClinic.class))).willReturn(serviceAtClinic);
+        BDDMockito.given(service.saveService(ArgumentMatchers.any(ServiceAtClinic.class))).willReturn(serviceAtClinic);
 
-    ObjectMapper objectMapper = new ObjectMapper();
+        ObjectMapper objectMapper = new ObjectMapper();
 
-    mockMvc.perform(MockMvcRequestBuilders.post("/api/services")
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .content(objectMapper.writeValueAsString(serviceAtClinic)))
-            .andExpect(status().isBadRequest());
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/services")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(serviceAtClinic)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("name").value("Name must be between 3 and 150 characters long!"));
 
-    Mockito.verify(service, Mockito.times(0)).saveService(ArgumentMatchers.any(ServiceAtClinic.class));
-  }
+        Mockito.verify(service, Mockito.times(0)).saveService(ArgumentMatchers.any(ServiceAtClinic.class));
+    }
 
-  @Test
-  @WithMockUser(roles = {"ADMIN", "VET"})
-  void deleteServiceById() throws Exception{
+    @Test
+    @WithMockUser
+    void saveServiceInvalidNameTest() throws Exception {
+        ServiceAtClinic serviceAtClinic = new ServiceAtClinic("][98432!@#$%^()_\\", "good service", new BigDecimal("10.5"));
 
-    long id = 1;
-    BDDMockito.given(service.existsServiceById(id)).willReturn(true);
-    BDDMockito.willDoNothing().given(service).deleteServiceById(id);
-    ResultActions response = mockMvc.perform(delete("/api/services/{id}", id));
+        BDDMockito.given(service.saveService(ArgumentMatchers.any(ServiceAtClinic.class))).willReturn(serviceAtClinic);
 
-    response.andExpect(status().isNoContent())
-            .andDo(MockMvcResultHandlers.print());
-  }
+        ObjectMapper objectMapper = new ObjectMapper();
 
-  @Test
-  @WithMockUser(roles = {"ADMIN", "VET"})
-  void deleteServiceTest_if404() throws Exception{
-    long id = 1;
-    BDDMockito.given(service.existsServiceById(id)).willReturn(false);
-    BDDMockito.willDoNothing().given(service).deleteServiceById(id);
-    ResultActions response = mockMvc.perform(delete("/api/services/{id}", id));
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/services")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(serviceAtClinic)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("name").value("Name must contain only letters, spaces, numbers and dashes!"));
 
-    response.andExpect(status().isNotFound())
-            .andDo(MockMvcResultHandlers.print());
-  }
+        Mockito.verify(service, Mockito.times(0)).saveService(ArgumentMatchers.any(ServiceAtClinic.class));
+    }
 
-  @Test
-  @WithMockUser(roles = {"USER"})
-  void deleteServiceTest_if403() throws Exception{
-    long id = 1;
-    BDDMockito.given(service.existsServiceById(id)).willReturn(false);
-    BDDMockito.willDoNothing().given(service).deleteServiceById(id);
-    ResultActions response = mockMvc.perform(delete("/api/services/{id}", id));
+    @Test
+    @WithMockUser
+    void saveServiceBadDescriptionTest() throws Exception {
+        ServiceAtClinic serviceAtClinic = new ServiceAtClinic("validName", """
+                According to all known laws of aviation, there is no way a bee should be able to fly.
+                Its wings are too small to get its fat little body off the ground.
+                The bee, of course, flies anyway because bees don't care what humans think is impossible.
+                Yellow, black. Yellow, black. Yellow, black. Yellow, black.
+                Ooh, black and yellow!
+                Let's shake it up a little.
+                Barry! Breakfast is ready!
+                Coming!
+                Hang on a second.
+                Hello?
+                Barry?
+                Adam?
+                Can you believe this is happening?
+                I can't.
+                I'll pick you up.
+                Looking sharp.
+                Use the stairs, Your father paid good money for those.
+                Sorry. I'm excited.
+                Here's the graduate.
+                We're very proud of you, son.
+                A perfect report card, all B's.
+                Very proud.
+                Ma! I got a thing going here.
+                You got lint on your fuzz.
+                Ow! That's me!
+                Wave to us! We'll be in row 118,000.
+                Bye!
+                Barry, I told you, stop flying in the house!
+                Hey, Adam.
+                Hey, Barry.
+                Is that fuzz gel?
+                A little. Special day, graduation.
+                Never thought I'd make it.
+                Three days grade school, three days high school.
+                Those were awkward.
+                Three days college. I'm glad I took a day and hitchhiked around The Hive.
+                You did come back different.
+                Hi, Barry. Artie, growing a mustache? Looks good.
+                Hear about Frankie?""",
+                new BigDecimal("10.5"));
 
-    response.andExpect(status().isForbidden())
-            .andDo(MockMvcResultHandlers.print());
-  }
+        BDDMockito.given(service.saveService(ArgumentMatchers.any(ServiceAtClinic.class))).willReturn(serviceAtClinic);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/services")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(serviceAtClinic)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("description").value("Description too long! Please limit it to a max of 255 characters!"));
+
+        Mockito.verify(service, Mockito.times(0)).saveService(ArgumentMatchers.any(ServiceAtClinic.class));
+    }
+
+    @Test
+    @WithMockUser(roles = {"ADMIN", "VET"})
+    void deleteServiceById() throws Exception {
+
+        long id = 1;
+        BDDMockito.given(service.existsServiceById(id)).willReturn(true);
+        BDDMockito.willDoNothing().given(service).deleteServiceById(id);
+        ResultActions response = mockMvc.perform(delete("/api/services/{id}", id));
+
+        response.andExpect(status().isNoContent())
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+    @Test
+    @WithMockUser(roles = {"ADMIN", "VET"})
+    void deleteServiceTest_if404() throws Exception {
+        long id = 1;
+        BDDMockito.given(service.existsServiceById(id)).willReturn(false);
+        BDDMockito.willDoNothing().given(service).deleteServiceById(id);
+        ResultActions response = mockMvc.perform(delete("/api/services/{id}", id));
+
+        response.andExpect(status().isNotFound())
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+    @Test
+    @WithMockUser(roles = {"USER"})
+    void deleteServiceTest_if403() throws Exception {
+        long id = 1;
+        BDDMockito.given(service.existsServiceById(id)).willReturn(false);
+        BDDMockito.willDoNothing().given(service).deleteServiceById(id);
+        ResultActions response = mockMvc.perform(delete("/api/services/{id}", id));
+
+        response.andExpect(status().isForbidden())
+                .andDo(MockMvcResultHandlers.print());
+    }
 }
 
 
