fcbb42f9d8a301cf7aebcdd5331289efb3d674c6
Edgaras Laptevas <edgaraslaptevas88gmail.com>
2025-03-07 12:33:32 +0200

    Authentification fix

--- api/src/main/java/SpringProject/_Spring/security/CorsConfig.java
+++ api/src/main/java/SpringProject/_Spring/security/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("https://localhost:5173/"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173/"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH"));
         configuration.setAllowedHeaders(List.of("Content-Type", "Authorization"));
         configuration.setExposedHeaders(List.of("X-Total-Count", "X-Request-Id"));
