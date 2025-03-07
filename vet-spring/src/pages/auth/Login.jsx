01a7b92bc3b7e30e7c655a18587544822d48ff36
Vladimir.Zorin <vladimir.zorin99@yahoo.com>
2025-03-07 16:40:52 +0400

    Mid day commit.

--- vet-spring/src/pages/auth/Login.jsx
+++ vet-spring/src/pages/auth/Login.jsx
@@ -1,63 +1,60 @@
-// import { useForm } from "react-hook-form";
-// import {NavLink} from "react-router";
-// import {useState} from "react";
-// import {useAuth} from "../../context/AuthContext.jsx";
-// import {Error} from "../../components/Error.jsx";
+import { useForm } from "react-hook-form";
+import {NavLink} from "react-router";
+import {useState} from "react";
+import {useAuth} from "../../context/AuthContext.jsx";
+import {Error} from "../../components/Error.jsx";
 
-// export const Login = () => {
-//     const { register, handleSubmit } = useForm();
-//     const [error, setError] = useState("")
-//     const { login } = useAuth()
+import Image from "../../assets/login-page-dog.png";
 
-//     const onSubmit = async (data) => {
-//         try {
-//             await login(data.email, data.password);
-//         } catch (error) {
-//             if (error.response.status === 401) {
-//                 setError("Invalid credentials")
-//             } else {
-//                 setError(error.response?.data?.message || error.message)
-//             }
-//         }
-//     };
+export const Login = () => {
+    const { register, handleSubmit } = useForm();
+    const [error, setError] = useState("")
+    const { login } = useAuth()
 
-//     return (
-//         <main className="grid place-items-center h-screen">
-//             <div className="flex flex-col gap-2 items-center">
-//                 <form onSubmit={handleSubmit(onSubmit)}>
-//                     <fieldset className="fieldset w-xs bg-base-200 border border-base-300 p-4 rounded-box">
-//                         <legend className="fieldset-legend">Login</legend>
+    const onSubmit = async (data) => {
+        try {
+            await login(data.email, data.password);
+        } catch (error) {
+            if (error.response.status === 401) {
+                setError("Invalid credentials")
+            } else {
+                setError(error.response?.data?.message || error.message)
+            }
+        }
+    };
 
-//                         <label className="fieldset-label">Email</label>
-//                         <input {...register("email")} type="text" className="input" placeholder="Enter email" />
+    return (
+        <main className="grid place-items-center h-screen">
+          {/* Center section: Form */}
+            <div className="flex flex-col gap-2 items-center">
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <fieldset className="fieldset w-xs bg-base-200 border border-base-300 p-4 rounded-box">
+                        <legend className="fieldset-legend">Login</legend>
 
-//                         <label className="fieldset-label">Password</label>
-//                         <input {...register("password")} type="password" className="input" placeholder="Enter password" />
+                        <label className="fieldset-label">Email</label>
+                        <input {...register("email")} type="text" className="input" placeholder="Enter email" />
 
-//                         <button type="submit" className="btn btn-primary mt-4">Login</button>
-//                         <NavLink to="/register" className="underline text-center mt-2">Register</NavLink>
-//                     </fieldset>
-//                 </form>
-//                 <Error error={error} isHidden={!error} />
-//             </div>
-//         </main>
-//     );
-// };
+                        <label className="fieldset-label">Password</label>
+                        <input {...register("password")} type="password" className="input" placeholder="Enter password" />
 
-import React from 'react';
-import {View, Text} from 'react-native';
-import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
+                        <button type="submit" className="btn btn-primary mt-4">Login</button>
+                        <NavLink to="/register" className="underline text-center mt-2">Register</NavLink>
+                    </fieldset>
+                </form>
+                <Error error={error} isHidden={!error} />
+            </div>
 
-const ViewBoxesWithColorAndText = () => {
-  return (
-    <SafeAreaProvider>
-      <SafeAreaView style={{height: 100, flexDirection: 'row'}}>
-        <View style={{backgroundColor: 'blue', flex: 0.2}} />
-        <View style={{backgroundColor: 'red', flex: 0.4}} />
-        <Text>Hello World!</Text>
-      </SafeAreaView>
-    </SafeAreaProvider>
-  );
+          {/* Right section: Image */}
+          <div className="hidden lg:block flex-1 bg-blue-500">
+            <img
+              src={Image} // Add your image URL here
+              alt="Login"
+              className="object-cover w-full h-full"
+            />
+          </div>
+
+        </main>
+
+        
+    );
 };
-
-export default ViewBoxesWithColorAndText;
