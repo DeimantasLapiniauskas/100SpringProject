ceacc44935a59b7f051a10e9f3d2ce12f7a8f55a
DeimantasLapiniauskas <deimislapinas@gmail.com>
2025-03-10 12:01:32 +0200

    fixed tests

--- api/src/main/java/SpringProject/_Spring/dto/client/ClientRequestDTO.java
+++ api/src/main/java/SpringProject/_Spring/dto/client/ClientRequestDTO.java
@@ -15,7 +15,7 @@ public record ClientRequestDTO(
 
         @NotNull(message = "Password can not be null!")
         @Length(min = 8, max = 50, message = "Your password is either too short or too long! Min length is 8, max is 50 symbols")
-        @Pattern(regexp = "^(?=(.+[a-zA-Z]))(?=(.+\\d))^[a-zA-Z0-9!\"#$%&'()*+,-./:;<=>?@^_`{|}~ ]+$\n",
+        @Pattern(regexp = "^(?=(.*[a-zA-Z]))(?=(.*\\d))[a-zA-Z0-9!\"#$%&'()*+,-./:;<=>?@^_`{|}~ ]*$",
                 message = "Your password must contain at least one number, one letter, and it only accepts those and the regular qwerty keyboard symbols!")
         // qwerty symbols: !"#$%&'()*+,-./:;<=>?@[\]^_{|}~`
         String password,
