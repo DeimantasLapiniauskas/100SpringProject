ea3cdc78947b7e88964771772a0c96e6e23beaa6
Vladimir.Zorin <vladimir.zorin99@yahoo.com>
2025-03-10 17:36:13 +0400

    App.css updated (3).

---
+++ vet-spring/src/pages/home/HomePage.jsx
@@ -0,0 +1,15 @@
+//import { useForm } from "react-hook-form";
+//import {NavLink} from "react-router";
+//import {useState} from "react";
+//import {useAuth} from "../../context/AuthContext.jsx";
+//import {Error} from "../../components/Error.jsx";
+
+//import { Navbar } from "../components/NavBar.jsx";
+
+export const HomePage = () => {
+    return (
+        <div className="home-page">
+            <p>Not implemented :c</p>
+        </div>
+    )
+}
