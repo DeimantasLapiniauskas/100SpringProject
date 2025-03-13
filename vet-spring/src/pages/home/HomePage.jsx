93d4ea802bb5d60fd9d24ea10c69e870edd8d7cb
Vladimir.Zorin <vladimir.zorin99@yahoo.com>
2025-03-13 22:14:56 +0400

    HomePage.jsx updated; Decided to try Tailwind and DaisyUI intead of Pure CSS; Better relationship with NavBar in screen size changing; App.css updated(new3); Updated heavily; have backup copy of Pure CSS just in case.

--- vet-spring/src/pages/home/HomePage.jsx
+++ vet-spring/src/pages/home/HomePage.jsx
@@ -12,26 +12,30 @@ import HomePageDog from "../../assets/dog.png";
 
 export const HomePage = () => {
     return (
-        <div className="home-page">
-            <main className="company-meet">
-                <figure>
-                    <img className="dog1" src={HomePageDog} alt="Dog; light brown and white fur; bent ears" />
+        <div className="min-h-screen bg-[#F5F4F4] flex justify-center items-center p-5">
+            <main className="flex flex-wrap justify-between items-start w-full max-w-screen-xl gap-8">
+                <figure className="flex-shrink-0 w-full sm:w-1/3">
+                    <img
+                        className="w-full max-w-[35vw] sm:max-w-[700px] h-auto object-cover"
+                        src={HomePageDog}
+                        alt="Dog; light brown and white fur; bent ears"
+                    />
                 </figure>
 
-                <div className="text-content">
-                    <article className="rustaveli-avenue">
+                <div className="text-content flex flex-col w-full sm:w-2/3 text-left pl-5 space-y-4">
+                    <article className="text-base text-black">
                         Pavasario Avenue 100, Vilnius<br />
-                         +370 511 233 78<br />
-                         Twenty-four hours a day
+                        +370 511 233 78<br />
+                        Twenty-four hours a day
                     </article>
 
-                    <h1 className="veterinary-company-in-georgia">
+                    <h1 className="text-4xl text-black">
                         Happy Hearts <br /> Veterinary clinic
                     </h1>
 
-                    <p className="as-georgia-leading">
-                        As Lithuania’s leading animal health company, Happy Hearts is
-                        driven by a singular purpose: to nurture Lithuania and
+                    <p className="text-base text-black leading-relaxed">
+                        As Lithuania’s leading animal health company, Happy Hearts is<br />
+                        driven by a singular purpose: to nurture Lithuania and<br />
                         humankind by advancing care for animals.
                     </p>
                 </div>
