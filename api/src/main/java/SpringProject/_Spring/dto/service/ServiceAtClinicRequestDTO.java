5182bbbdbaeb0dfead71c192f80848c94cfc6f08
DeimantasLapiniauskas <deimislapinas@gmail.com>
2025-03-18 09:10:40 +0200

    fixed regex in client phone number and service, updated client phone number message

--- api/src/main/java/SpringProject/_Spring/dto/service/ServiceAtClinicRequestDTO.java
+++ api/src/main/java/SpringProject/_Spring/dto/service/ServiceAtClinicRequestDTO.java
@@ -7,7 +7,7 @@ import java.math.BigDecimal;
 public record ServiceAtClinicRequestDTO(
         //@NotBlank is not needed when we've got a minimum size already.
         @Size(min = 3, max = 150, message = "Name must be between 3 and 150 characters long!")
-        @Pattern(regexp = "^[A-Za-z0-9\\s-]+$", message = "Name must contain only letters, spaces, numbers and dashes!")
+        @Pattern(regexp = "^[A-Za-z0-9\\s-]*$", message = "Name must contain only letters, spaces, numbers and dashes!")
         String name,
 
         @NotBlank
