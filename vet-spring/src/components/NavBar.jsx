7d7fba51945a4f219274346af4e25766d839caf1
Vladimir.Zorin <vladimir.zorin99@yahoo.com>
2025-03-19 19:10:18 +0400

    NavBar.jsx updated; Home route added; 'Home', 'Your Pets', 'Service List' text typography updated test NEW Typography in index.css.

--- vet-spring/src/components/NavBar.jsx
+++ vet-spring/src/components/NavBar.jsx
@@ -7,8 +7,9 @@ export const Navbar = () => {
     return (
         <div>
             <nav className="lg:h-[90px] md:h-[73px] h-[56px] bg-[#6A7AFF] px-[5rem] flex justify-between items-center rounded-[10px] border-2 border-[#CBC5C5]">
-                <NavLink to={"/pets"}><p className="text-white">Your Pets</p></NavLink>
-                <NavLink to={"/services"}><p className="text-white">Service list</p></NavLink>
+                <NavLink to={"/home"}><p className="figma-headline-4 text-white">Home</p></NavLink>
+                <NavLink to={"/pets"}><p className="figma-headline-4 text-white">Your Pets</p></NavLink>
+                <NavLink to={"/services"}><p className="figma-headline-4 text-white">Service list</p></NavLink>
                 {account ?
                 <button type="button" value="logout" onClick={logout} className="custom-purple-btn cursor-pointer">Logout</button> :
                 <NavLink to={"/login"}><p><button className="custom-purple-btn cursor-pointer">Login</button></p></NavLink>}
