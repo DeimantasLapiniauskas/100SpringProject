a1efe5129ca4c24b20a18c6916cbf96c109b3c8e
DeimantasLapiniauskas <deimislapinas@gmail.com>
2025-03-25 16:31:36 +0200

    temporary commit

--- api/src/main/java/SpringProject/_Spring/validation/customAnnotations/authentication/email/EmailRegexValidator.java
+++ api/src/main/java/SpringProject/_Spring/validation/customAnnotations/authentication/email/EmailRegexValidator.java
@@ -10,6 +10,9 @@ public class EmailRegexValidator implements ConstraintValidator<EmailRegex,Strin
         return email == null || // allow null, because we already have @NotNull checking it, and we want to show that message on receiving a null instead of the one this is associated with. Also String.matches() throws an error if the string is null.
                 email.trim().length() <= EmailLengthValidator.minLength ||
                 email.length() >= EmailLengthValidator.maxLength || // allow those that don't pass EmailLengthValidator, so that EmailLengthValidator's message has a higher priority.
-                email.matches("^[a-zA-Z0-9._%+-]+(?!(.*[.]{2,}|.*@{2,}))[a-zA-Z0-9.-]{3,}\\.[a-zA-Z]{2,}$");
+                email.matches("^[a-zA-Z0-9._%+-]+" +
+                        "(?!.*(.*[.]{2,}|.*@{2,}))[a-zA-Z0-9.-]{3,}" +
+                        "\\.[a-zA-Z]{2,}$");
+        //                           ^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$
     }
 }
