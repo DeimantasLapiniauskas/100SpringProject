d83c7709c5bbc6d30f39674445992843f2ba2138
Andrej Titkov <ronya.titkov@gmail.com>
2025-03-28 10:01:44 +0200

    Fix typos in VetController, add ClientController and DTOs for it

--- api/src/main/java/SpringProject/_Spring/controller/VetController.java
+++ api/src/main/java/SpringProject/_Spring/controller/VetController.java
@@ -5,6 +5,7 @@ import SpringProject._Spring.dto.authentication.vet.VetPageResponseDTO;
 import SpringProject._Spring.dto.vet.VetMapping;
 import SpringProject._Spring.model.authentication.Vet;
 import SpringProject._Spring.service.authentication.VetService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
@@ -15,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/")
+@RequestMapping("/api")
 public class VetController extends BaseController {
 
     private final VetService vetService;
@@ -25,18 +26,18 @@ public class VetController extends BaseController {
         this.vetService = vetService;
     }
 
-    //@Operation(summary = "Get pets by owner ID and split them by pages (Client and Admin)", description = "Retrieves all pets owned by client by his ID and splits the list by pages")
-    @GetMapping("/client/pagination")
-    @PreAuthorize("hasAuthority('SCOPE_ROLE_CLIENT') or hasAuthority('SCOPE_ROLE_ADMIN')")
-    public ResponseEntity<ApiResponse<VetPageResponseDTO>> getAllPostsPage(@RequestParam int page,
-                                                                           @RequestParam int size,
-                                                                           @RequestParam(required = false) String sort) {
+    @Operation(summary = "Get vets for admin page (Admin)", description = "Retrieves all vets and splits the list by pages")
+    @GetMapping("/vet/pagination")
+    @PreAuthorize("hasAuthority('SCOPE_ROLE_ADMIN')")
+    public ResponseEntity<ApiResponse<VetPageResponseDTO>> getAllVetsPage(@RequestParam int page,
+                                                                          @RequestParam int size,
+                                                                          @RequestParam(required = false) String sort) {
         if (page < 0 || size <= 0) {
             throw new IllegalArgumentException("Invalid page or size parameters");
         }
 
         Page<Vet> pagedVets = vetService.findAllPetsPage(page, size, sort);
-        String message = pagedVets.isEmpty() ? "Vey list is empty" : null;
+        String message = pagedVets.isEmpty() ? "Vet list is empty" : null;
         VetPageResponseDTO responseDTO = VetMapping.toVetPageResponseDTO(pagedVets);
 
         return ok(responseDTO, message);
