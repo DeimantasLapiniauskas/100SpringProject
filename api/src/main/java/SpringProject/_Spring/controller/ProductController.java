5bef84eddaa8f80b7a97afdcc442dfe655a9ac81
Andrej Titkov <ronya.titkov@gmail.com>
2025-04-03 14:35:43 +0300

    Add Product controller, service, repository

---
+++ api/src/main/java/SpringProject/_Spring/controller/ProductController.java
@@ -0,0 +1,18 @@
+package SpringProject._Spring.controller;
+
+import SpringProject._Spring.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ProductController {
+
+    private final ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+}
---
+++ api/src/main/java/SpringProject/_Spring/service/ProductService.java
@@ -0,0 +1,18 @@
+package SpringProject._Spring.service;
+
+import SpringProject._Spring.repository.ProductRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+}
