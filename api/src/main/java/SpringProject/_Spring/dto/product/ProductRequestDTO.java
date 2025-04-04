79b8467e479730c86382894c9e3a0a339bb685cc
Andrej Titkov <ronya.titkov@gmail.com>
2025-04-04 12:38:48 +0300

    Add validation for ProductRequestDTO

--- api/src/main/java/SpringProject/_Spring/dto/product/ProductRequestDTO.java
+++ api/src/main/java/SpringProject/_Spring/dto/product/ProductRequestDTO.java
@@ -1,9 +1,35 @@
 package SpringProject._Spring.dto.product;
 
+import jakarta.validation.constraints.*;
+
 import java.math.BigDecimal;
 
-public record ProductRequestDTO(String name,
+public record ProductRequestDTO(@NotBlank(message = "Name can't be empty!")
+                                @Pattern(
+                                        regexp = "^[a-zA-Z0-9.,!?@#$%^&*()'\"]+$",
+                                        message = "Name can only contain letters, numbers, punctuation, quotes, and symbols (!@#$%^&*())"
+                                )
+                                String name,
+                                @NotBlank(message = "Description can't be empty!")
+                                @Pattern(
+                                        regexp = "^[a-zA-Z0-9.,!?@#$%^&*()'\"]+$",
+                                        message = "Description can only contain letters, numbers, punctuation, quotes, and symbols (!@#$%^&*())"
+                                )
                                 String description,
+                                @NotNull(message = "Price can't be null!")
+                                @DecimalMin(
+                                        value = "0.00",
+                                        message = "Price must be zero or positive!"
+                                )
+                                @Digits(
+                                        integer = 10,
+                                        fraction = 2,
+                                        message = "Price must have up to 10 integer digits and 2 fractional digits!"
+                                )
                                 BigDecimal price,
+                                @Min(
+                                        value = 0,
+                                        message = "Stock quantity must be zero or greater!"
+                                )
                                 int stockQuantity) {
 }
