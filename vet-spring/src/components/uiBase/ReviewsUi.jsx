8496fd9813714c934e3ae80a4e2c2dae8fba4bd0
Edgaras Laptevas <edgaraslaptevas88gmail.com>
2025-04-06 15:15:44 +0300

    small changes to design

--- vet-spring/src/components/uiBase/ReviewsUi.jsx
+++ vet-spring/src/components/uiBase/ReviewsUi.jsx
@@ -5,7 +5,7 @@ export const Reviews = () => {
     <div className="flex flex-col justify-center items-center  bg-gradient-to-b from-blue-500 via-info to-blue-500 rounded-[15px] px-4 py-2 sm:py-4 md:py-6 lg:py-8 gap-1">
       <img src={reviewers} alt="reviewers" className="w-25 sm:w-50" />
       <p className="text-info-content font-semibold">101 Reviews</p>
-      <p className="text-white text-[10px] sm:text-xs md:text-sm hover:underline">Read here</p>
+      <p className="text-white text-[10px] sm:text-xs md:text-sm hover:underline">Leave your feedback</p>
     </div>
   );
 };
