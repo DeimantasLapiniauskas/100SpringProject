2016e315156a6a22bf853376cfe675c64ca5b306
Simas.Padvariskis <s.padvariskis@gmail.com>
2025-04-16 14:46:14 +0300

    Created Cart model with DTO foundation

---
+++ api/src/main/java/SpringProject/_Spring/dto/cart/CartResponseDTO.java
@@ -0,0 +1,13 @@
+package SpringProject._Spring.dto.cart;
+
+import SpringProject._Spring.model.cart.CartItem;
+
+import java.util.List;
+import java.sql.Timestamp;
+
+public record CartResponseDTO(
+    long id,
+    long clientId,
+    List<CartItem> cartItems,
+    Timestamp createdAt
+) { }
---
+++ api/src/main/java/SpringProject/_Spring/model/cart/Cart.java
@@ -0,0 +1,70 @@
+package SpringProject._Spring.model.cart;
+
+import SpringProject._Spring.model.Product;
+import jakarta.persistence.*;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "carts")
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column (name = "client_id")
+    private long clientId;
+
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL)
+    private List<CartItem> cartItems = new ArrayList<>();
+
+    private Timestamp createdAt;
+
+    public Cart(long id, long clientId, List<CartItem> cartItems, Timestamp createdAt) {
+        this.id = id;
+        this.clientId = clientId;
+        this.cartItems = cartItems;
+        this.createdAt = createdAt;
+    }
+
+    public Cart() {
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(long clientId) {
+        this.clientId = clientId;
+    }
+
+    public List<CartItem> getCartItems() {
+        return cartItems;
+    }
+
+    public void setCartItems(List<CartItem> cartItems) {
+        this.cartItems = cartItems;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+}
