2558f33dbf47cfbce6dba916b8701e85b64c2ca3
noonforme <d3jp@TECHIN.INTERNAL>
2025-04-16 11:35:07 +0300

    Fixed auth error while pulling pets as Vet

--- api/src/main/java/SpringProject/_Spring/controller/PetController.java
+++ api/src/main/java/SpringProject/_Spring/controller/PetController.java
@@ -145,7 +145,7 @@ public class PetController extends BaseController {
 
     @Operation(summary = "Get pets by owner ID and split them by pages (Client and Admin)", description = "Retrieves all pets owned by client by his ID and splits the list by pages")
     @GetMapping("/pagination")
-    @PreAuthorize("hasAuthority('SCOPE_ROLE_CLIENT') or hasAuthority('SCOPE_ROLE_ADMIN')")
+    @PreAuthorize("hasAuthority('SCOPE_ROLE_CLIENT') or hasAuthority('SCOPE_ROLE_ADMIN') or hasAuthority('SCOPE_ROLE_VET')")
     public ResponseEntity<ApiResponse<PetPageResponseDTO>> getAllPostsPage(Authentication authentication,
                                                                            @RequestParam int page,
                                                                            @RequestParam int size,
