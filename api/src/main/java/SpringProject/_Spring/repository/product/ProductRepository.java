e24965c07450e9125b416a6714da36007e6a8940
Andrej Titkov <ronya.titkov@gmail.com>
2025-04-16 15:13:28 +0300

    Add searchbar and sorting to Product Page, fix bugs/finished security

--- api/src/main/java/SpringProject/_Spring/repository/product/ProductRepository.java
+++ api/src/main/java/SpringProject/_Spring/repository/product/ProductRepository.java
@@ -2,12 +2,61 @@ package SpringProject._Spring.repository.product;
 
 
 import SpringProject._Spring.model.product.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
+    @Query(value = """
+                SELECT p FROM Product p
+                WHERE LOWER(p.name) LIKE LOWER(CONCAT('%', :search, '%'))
+                ORDER BY p.id DESC
+            """,
+            countQuery = """
+                        SELECT COUNT(p) FROM Product p
+                        WHERE LOWER(p.name) LIKE LOWER(CONCAT('%', :search, '%'))
+                    """)
+    Page<Product> searchByName(@Param("search") String search, Pageable pageable);
+
+    @Query(value = """
+                SELECT p FROM Product p
+                JOIN p.categories c
+                WHERE LOWER(p.name) LIKE LOWER(CONCAT('%', :search, '%'))
+                AND c.name = :categoryName
+                ORDER BY p.id DESC
+            """,
+            countQuery = """
+                        SELECT COUNT(p) FROM Product p
+                        JOIN p.categories c
+                        WHERE LOWER(p.name) LIKE LOWER(CONCAT('%', :search, '%'))
+                        AND c.name = :categoryName
+                    """)
+    Page<Product> searchInCategory(
+            @Param("search") String search,
+            @Param("categoryName") String categoryName,
+            Pageable pageable
+    );
+
+    @Query(value = """
+                SELECT p FROM Product p
+                JOIN p.categories c
+                WHERE c.name = :categoryName
+                ORDER BY p.id DESC
+            """,
+            countQuery = """
+                        SELECT COUNT(p) FROM Product p
+                        JOIN p.categories c
+                        WHERE c.name = :categoryName
+                    """)
+    Page<Product> findByCategory(
+            @Param("categoryName") String categoryName,
+            Pageable pageable
+    );
 
     boolean existsProductByName(String name);
 }
--- vet-spring/src/pages/products/ProductCard.jsx
+++ vet-spring/src/pages/products/ProductCard.jsx
@@ -2,45 +2,47 @@ import ModalContext from "@/utils/helpers/modalContext";
 import { useContext } from "react";
 import ProductModal from "./ProductModal";
 import EditProductButton from "./buttons/EditProductButton";
+import { useCheckRoles } from "@/hooks/useCheckRoles";
 
 const ProductCard = ({ product, getPage, currentPage, pageSize }) => {
   const { name, description, price, stockQuantity, id, imageUrl } = product;
   const { activeModalID, setModalID } = useContext(ModalContext);
 
+  const roles = useCheckRoles();
   return (
     <>
-      <li
-        className="border-b border-gray-200 hover:bg-gray-100 transition-colors duration-200 cursor-pointer"
-        onClick={() => setModalID(id.toString())}
-      >
-        <div className="flex items-center py-4 px-6 md:px-4 md:py-2 gap-4">
-          <div className="w-16 h-16 bg-gray-200 rounded-lg flex items-center justify-center">
-            {imageUrl ? (
+      <div>
+        <li
+          className="border-b bg-gray-100 rounded-2xl border-[1px] border-[#2c70b1] hover:bg-gray-50 transition-colors duration-200 cursor-pointer"
+          onClick={() => setModalID(id.toString())}
+        >
+          <div className="flex items-center py-4 px-6 md:px-4 md:py-2 gap-4">
+            <div className="w-[15rem] h-[10rem] bg-gray-200 rounded-lg flex items-center justify-center">
               <img src={imageUrl} alt={name} className="w-full h-full object-cover rounded-lg" />
-            ) : (
-              <span className="text-gray-500">No Image</span>
-            )}
-          </div>
-          <div className="w-full">
-            <div className="w-full text-lg md:text-base font-medium flex justify-between">
-              <div>{name}</div>
-              <div>{price} EUR</div>
             </div>
-            <div className="text-gray-700">
-              <p>{description}</p>
-              <p>Quantity: {stockQuantity}</p>
-            </div>
-            <div className="mt-2">
-              <EditProductButton
-                product={product}
-                getPage={getPage}
-                currentPage={currentPage}
-                pageSize={pageSize}
-              />
+            <div className="w-full h-[8rem]">
+              <div className="w-full text-lg md:text-base font-medium flex justify-between">
+                <div className="font-bold">{name}</div>
+                <div>{price} EUR</div>
+              </div>
+              <div className="text-gray-700">
+                <p>{description}</p>
+                <p>Quantity: {stockQuantity}</p>
+              </div>
             </div>
           </div>
-        </div>
-      </li>
+        </li>
+        {roles && (
+                <div className="mt-2">
+                  <EditProductButton
+                    product={product}
+                    getPage={getPage}
+                    currentPage={currentPage}
+                    pageSize={pageSize}
+                  />
+                </div>
+              )}
+      </div>
 
       {activeModalID === id.toString() && (
         <ProductModal product={product} onClose={() => setModalID("")} />
