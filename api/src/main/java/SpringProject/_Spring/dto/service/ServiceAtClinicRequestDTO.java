695d6b7eee4ce4eda1cf665e2aa548f886bcb055
DeimantasLapiniauskas <deimislapinas@gmail.com>
2025-04-16 13:29:24 +0300

    added a new, special apostrophe to the regex

--- api/src/main/java/SpringProject/_Spring/dto/service/ServiceAtClinicRequestDTO.java
+++ api/src/main/java/SpringProject/_Spring/dto/service/ServiceAtClinicRequestDTO.java
@@ -12,7 +12,7 @@ public record ServiceAtClinicRequestDTO(
         @NotBlank
         @NotNull
         @Size(max = 255, message = "Description too long! Please limit it to a max of 255 characters!")
-        @Pattern(regexp = "^[A-Za-z0-9\\s.\\-?!',]*$",
+        @Pattern(regexp = "^[A-Za-z0-9\\s.\\-?!',’]*$",
                 message = "Description must contain only letters, spaces, numbers, fullstops," +
                         " apostrophes, commas, exclamation and question marks and dashes!")
         String description,
