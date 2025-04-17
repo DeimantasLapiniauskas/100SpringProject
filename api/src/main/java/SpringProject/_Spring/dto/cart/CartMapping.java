8c0d3724feb407d3c5789670aeaf6fc72b57a67f
Simas.Padvariskis <s.padvariskis@gmail.com>
2025-04-17 12:42:51 +0300

    updated KOM-146

---
+++ api/src/main/java/SpringProject/_Spring/dto/cart/CartMapping.java
@@ -0,0 +1,12 @@
+package SpringProject._Spring.dto.cart;
+
+
+import SpringProject._Spring.model.cart.Cart;
+
+
+public class CartMapping {
+    public static CartResponseDTO toCartResponseDTO(Cart cart) {
+        return new CartResponseDTO(cart.getId(), cart.getClientId(), cart.getCartItems(), cart.getCreatedAt());
+    }
+
+}
