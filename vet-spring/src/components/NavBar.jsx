850136b91608e36c13e755b5c9704e7b3b1f33db
RadZil <radvilas.zilinskas@gmail.com>
2025-04-17 13:45:53 +0300

    nav changes

--- vet-spring/src/components/NavBar.jsx
+++ vet-spring/src/components/NavBar.jsx
@@ -1,30 +1,36 @@
 import { NavLink } from "react-router";
 import { DropdownMenu } from "../pages/profile/DropdownMenu";
 import menu from "../assets/icons/pawsNav.png";
+import defaultProfile from "../assets/icons/user.png";
 export const Navbar = () => {
   
   return (
     <div>
-      <nav className="lg:h-[84px] md:h-[72px] sm:h-[60px] h-[48px] bg-gradient-to-br from-blue-400 to-indigo-600 px-[1rem] sm:px-[3rem] md:px-[5rem] flex justify-between items-center rounded-[10px] border-2 border-white shadow-lg shadow-white max-w-[1500px] mx-auto">
+      <nav className="lg:h-[84px] md:h-[72px] sm:h-[60px] h-[48px] bg-gradient-to-br from-indigo-100 to-blue-100 px-[1rem] sm:px-[3rem] md:px-[5rem] flex justify-between items-center rounded-[100px] border-2 border-white shadow-md shadow-black max-w-[1500px] mx-auto">
+        <div>
+        <img src={menu} alt="" className="w-15" />
+        </div>
+      
+        
         <NavLink to={"/home"}
-         className={({ isActive }) => isActive? "text-[#005050] font-semibold hover:animate-pulse" : "inline-block transform transition duration-400 hover:-translate-y-1 text-white"}>
+         className={({ isActive }) => isActive? "text-[#005050] text-decoration-line: underline font-semibold hover:animate-pulse" : "inline-block transform transition duration-400 hover:-translate-y-1 text-black"}>
           <p className="text-xs sm:text-sm md:text-base">Home</p>
         </NavLink>
         <NavLink to={"/posts"}
-        className={({ isActive }) => isActive? "text-[#005050] font-semibold hover:animate-pulse" : "inline-block transform transition duration-400 hover:-translate-y-1 text-white"}>
+        className={({ isActive }) => isActive? "text-[#005050] text-decoration-line: underline font-semibold hover:animate-pulse" : "inline-block transform transition duration-400 hover:-translate-y-1 text-black"}>
           <p className="text-xs sm:text-sm md:text-base">News</p>
         </NavLink>
         <NavLink to={"/services"}
-         className={({ isActive }) => isActive? "text-[#005050] font-semibold hover:animate-pulse" : "inline-block transform transition duration-400 hover:-translate-y-1 text-white"}>
+         className={({ isActive }) => isActive? "text-[#005050] text-decoration-line: underline font-semibold hover:animate-pulse" : "inline-block transform transition duration-400 hover:-translate-y-1 text-black"}>
           <p className="text-xs sm:text-sm md:text-base">Service List</p>
         </NavLink>
         <NavLink to={"/products"} 
-         className={({ isActive }) => isActive? "text-[#005050] font-semibold hover:animate-pulse" : "inline-block transform transition duration-400 hover:-translate-y-1 text-white"}>
+         className={({ isActive }) => isActive? "text-[#005050] text-decoration-line: underline font-semibold hover:animate-pulse" : "inline-block transform transition duration-400 hover:-translate-y-1 text-black"}>
           <p className="text-xs sm:text-sm md:text-base">Products</p>
         </NavLink>
         <div className="dropdown">
-          <button className="dropbtn btn bg-[#97a0f1] w-12">
-            <img src={menu} alt="" className="w-10 absolute" />
+          <button className="bg-indigo-300 rounded-full w-12 h-12 shadow-md shadow-white justify-center hover:animate-pulse inline-block transform transition duration-400 hover:-translate-y-1 text-white">
+          <img src={defaultProfile} alt="" className="rounded-full overflow-hidden w-full h-full object-cover"></img>
             <i className="fa fa-caret-down mb-10"></i>
           </button>
           <div className="dropdown-content flex flex-col ml-[-30px]">
