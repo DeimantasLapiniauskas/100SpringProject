67579af7a5b327f415fc980fed5fc56d2ae3c340
Vladimir.Zorin <vladimir.zorin99@yahoo.com>
2025-04-23 13:07:21 +0300

    Added 1 more Atsarginis(Insert File Name) files inside designtest folder.

---
+++ vet-spring/src/pages/designtest/AtsarginisMainLayout.jsx
@@ -0,0 +1,19 @@
+// import {Outlet} from "react-router";
+// import { Navbar } from "../components/NavBar.jsx";
+// import { DeletePostModal } from "@/pages/posts/DeleteModal.jsx";
+
+// export const MainLayout = () => {
+    
+//     return (
+//         <div className="p-2">
+//             <header className="pb-1">
+//                 <Navbar/>
+//             </header>
+//             <main >
+//                 {/* gal geriau kad */}
+//                 <Outlet />
+//                 <DeletePostModal/>
+//             </main>
+//         </div>
+//     )
+// }
