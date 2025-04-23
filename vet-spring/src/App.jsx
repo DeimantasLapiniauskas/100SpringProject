e5da7bb5c19ec3a82d16b9b6266db954a8c34592
Vladimir.Zorin <vladimir.zorin99@yahoo.com>
2025-04-23 13:45:39 +0300

    App.jsx addition: pawss images as background using tailwind bg-repeat .

--- vet-spring/src/App.jsx
+++ vet-spring/src/App.jsx
@@ -2,12 +2,22 @@
 import "./App.css";
 import VetClinicRoutes from "./components/VetClinicRoutes";
 import { CustomToaster } from "@/components/uiBase/CustomToaster";
+import pawssBackgroundImage from "/src/assets/icons/pawss_for_background_spaced_out_rotated_1536px.png";
 
 const App = () => {
   return (
-    <div className="px-5 pb-5">
+    <div className="relative w-full min-h-screen">
+      {/* Paws background globally */}
+      <div
+        className="absolute inset-0 bg-repeat bg-[length:3rem_3rem] sm:bg-[length:3rem_3rem] md:bg-[length:6rem_6rem] lg:bg-[length:9rem_9rem] bg-center opacity-5 pointer-events-none z-0"
+        style={{ backgroundImage: `url(${pawssBackgroundImage})` }}
+      ></div>
+
+      {/* Content sits above paw background */}
+      <div className="relative z-10 px-5 pb-5">
         <CustomToaster reverseOrder={false} />
         <VetClinicRoutes />
+      </div>
     </div>
   );
 };
