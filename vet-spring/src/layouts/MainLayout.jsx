96abc32aab1239607ab1a39b7c59253e41b6f24f
Vladimir.Zorin <vladimir.zorin99@yahoo.com>
2025-04-23 13:24:22 +0300

    MainLayout.jsx addition: pawss images as background using tailwind bg-repeat .

--- vet-spring/src/layouts/MainLayout.jsx
+++ vet-spring/src/layouts/MainLayout.jsx
@@ -2,18 +2,28 @@ import {Outlet} from "react-router";
 import { Navbar } from "../components/NavBar.jsx";
 import { DeletePostModal } from "@/pages/posts/DeleteModal.jsx";
 
+import pawssBackgroundImage from "/src/assets/icons/pawss_for_background_spaced_out_rotated_1536px.png";
+
 export const MainLayout = () => {
     
     return (
-        <div className="p-2">
-            <header className="pb-1">
-                <Navbar/>
-            </header>
-            <main >
-                {/* gal geriau kad */}
-                <Outlet />
-                <DeletePostModal/>
-            </main>
+        <div className="relative w-full min-h-screen">
+        {/* Paws background */}
+        <div
+          className="absolute inset-0 bg-repeat bg-[length:3rem_3rem] sm:bg-[length:3rem_3rem] md:bg-[length:6rem_6rem] lg:bg-[length:9rem_9rem] bg-center opacity-5 pointer-events-none z-0"
+          style={{ backgroundImage: `url(${pawssBackgroundImage})` }}
+        ></div>
+    
+        {/* Content wrapper above background */}
+        <div className="relative z-10 p-2">
+          <header className="pb-1">
+            <Navbar />
+          </header>
+          <main>
+            <Outlet />
+            <DeletePostModal />
+          </main>
         </div>
-    )
-}
+      </div>
+    );
+};
