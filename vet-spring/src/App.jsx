c65ffc036a1a1f6745459b34022b4a5738ca0473
Vladimir.Zorin <vladimir.zorin99@yahoo.com>
2025-04-27 20:54:21 +0300

    App.jsx addition: relative added to <div> above <CustomToaster reverseOrder={false} /> to prevent pawss images showing on NavBar.

--- vet-spring/src/App.jsx
+++ vet-spring/src/App.jsx
@@ -7,7 +7,7 @@ import pawssBackgroundImage from "/src/assets/icons/pawss_for_background_spaced_
 const App = () => {
   return (
     <div className="relative w-full min-h-screen">
-      
+
       {/* Paws background globally */}
       <div
         className="absolute inset-0 bg-repeat bg-[length:3rem_3rem] sm:bg-[length:3rem_3rem] md:bg-[length:6rem_6rem] lg:bg-[length:9rem_9rem] bg-[center_top_0.1rem] opacity-5 pointer-events-none z-0"
@@ -15,7 +15,7 @@ const App = () => {
       ></div>
 
       {/* Content sits above paw background */}
-      <div className="px-2 pb-2">
+      <div className="relative px-2 pb-2">
         <CustomToaster reverseOrder={false} />
         <VetClinicRoutes />
       </div>
