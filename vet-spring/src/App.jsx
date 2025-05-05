cae4293c0bec2dffd19b6bc133a26de5982a284d
Vladimir.Zorin <vladimir.zorin99@yahoo.com>
2025-05-05 15:26:41 +0300

    Update branch(2).

--- vet-spring/src/App.jsx
+++ vet-spring/src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
     <div className="relative min-h-screen">
       {/* Above className has to have "relative" and "min-h-screen" as it ensures paw print images cover whole page. Not half or less. */}
 
-      {/* Ensures paw print image background is applied globally */}
+      {/* Ensures paw print image background is applied globally. */}
       <div
         className="absolute inset-0 bg-repeat bg-[length:3rem_3rem] sm:bg-[length:3rem_3rem] md:bg-[length:6rem_6rem] lg:bg-[length:9rem_9rem] bg-[center_top_0.1rem] opacity-5 pointer-events-none z-0"
         style={{ backgroundImage: `url(${pawssBackgroundImage})` }}
